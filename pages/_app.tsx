e10911728a47c0ec602649984822ff2144cbdff8
Richard Barkinskiy <richard@phillyfusion.gg>
2022-06-10 09:32:32 -0700

    gtag added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
 import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-3HQXFTBL36"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-3HQXFTBL36');
+        `}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,6 @@ import Document, {
   NextScript,
   DocumentContext,
 } from "next/document";
-import Script from "next/script";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -21,19 +20,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Barlow&family=Joan&display=swap"
             rel="stylesheet"
           />
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-3HQXFTBL36"
-            strategy="afterInteractive"
-          />
-          <Script id="google-analytics" strategy="afterInteractive">
-            {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){window.dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-3HQXFTBL36');
-        `}
-          </Script>
         </Head>
         <body>
           <Main />
